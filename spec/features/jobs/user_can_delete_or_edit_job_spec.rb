8d2f828045a526a75efb1c225f92b6aa597b29d3
Mike Heft <mikeheft@gmail.com>
2017-09-02 11:02:30 -0600

    refactor tests to show changes made to links

--- spec/features/jobs/user_can_delete_or_edit_job_spec.rb
+++ spec/features/jobs/user_can_delete_or_edit_job_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.feature "A User can" do
   scenario "delete a record for a job" do
     company = create(:company)
-    job_1, job_2, job_3 = create_list(:job, 3)
-    expect(Job.count).to eq(3)
-    visit company_job_path(company, job_1)
-    click_on('Delete Job Record')
+    company.jobs = create_list(:job, 3)
+    expect(company.jobs.count).to eq(3)
+    visit company_job_path(company, company.jobs.first)
+    click_on('Delete Job Record', match: :first)
     
-    expect(Job.count).to eq(2)
+    expect(company.jobs.count).to eq(2)
   end
   
   scenario "edit an existing job record" do
     job = create(:job)
-    visit company_job_path(Company.first, job)
-    click_on("Edit Job Record")
+    visit company_job_path(job.company, job)
+    click_on("Edit Job Record", match: :first)
     
-    expect(current_path).to eq(edit_company_job_path(Company.first, job))
+    expect(current_path).to eq(edit_company_job_path(job.company, job))
     
     fill_in('Title:', with: "Engi")
     fill_in('Desc:', with: 'Goblin')
