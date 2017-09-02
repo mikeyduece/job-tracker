858148d20a24b90bc945e58dff67ac7b9ecb3cf2
Mike Heft <mikeheft@gmail.com>
2017-09-02 13:43:03 -0600

    Add when comment was created and updated

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :job
+  
+  def created
+    Date.strptime(self.created_at.to_s, "%Y-%m-%d %H:%M:%S %Z")
+  end
+  
+  def updated
+    Date.strptime(self.updated_at.to_s, "%Y-%m-%d %H:%M:%S %Z")
+  end
 end
