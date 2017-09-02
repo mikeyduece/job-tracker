8607fda8b09788b804b62e8c4a07a2df284ac404
Mike Heft <mikeheft@gmail.com>
2017-09-02 10:48:45 -0600

    Fix error of unassigned category_id for a job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,9 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
-  belongs_to :category
+  belongs_to :category, optional: true
+  
+  def no_category
+      "No Category Assigned"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  root to: 'companies#index'
+  
   resources :companies do
     resources :jobs
   end
